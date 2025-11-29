3e74ffa6e81fc33563a2ae1838febac70168a660
Guo-Hua Wang <13559338+DoctorKey@users.noreply.github.com>
2025-11-29 18:00:58 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+import os
+
+setup(
+    name='ovis_image',
+    version='1.0.0',
+    packages=find_packages(include=['ovis_image']),
+    # include any other necessary details here
+)
